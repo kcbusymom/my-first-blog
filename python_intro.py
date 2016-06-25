3594a445cbd616daefeef2fec1a3f5667bd6c08a
Heather <kcbusymom@gmail.com>
2016-06-25 13:34:50 -0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+volume = 27
+if volume < 20:
+	print("It's kinda quiet.")
+elif 20 <=volume < 40:
+	print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+	print("Nice for parties")
+elif 80 <= volume < 100:
+	print("A bit loud!")
+else:
+	print("My ears are hurting! :(")
